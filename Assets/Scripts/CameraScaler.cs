dd1302cf630a68de426d80a912dfdc7594b12cbd
BrettHuang <Brett@edokiacademy.com>
2023-05-01 13:06:39 -0400

    Add camera scaler for iphone and ipad

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+    void Start()
+    {
+        bool deviceIsIpad = UnityEngine.iOS.Device.generation.ToString().Contains("iPad");
+        if (deviceIsIpad)
+        {
+            transform.localPosition = new Vector3(-3.3f, 0.3f, -11);
+            GetComponent<Camera>().orthographicSize = 2.3f;
+        }
+    }
+
+
+    
+}
